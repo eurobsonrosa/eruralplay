4cdb10f3b2012fc2be15b57f42220ffa6aa456b6
Robson Rosa <contatorobsonrosa@gmail.com>
2022-10-21 19:41:05 -0300

    add message component

---
+++ frontend/src/components/message/MainMessage.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="message-content" :style="{backgroundColor: bgColor}">
+    <span class="message-text">
+      <strong v-if="message.username">{{ message.username }}: </strong> {{ message.text }}
+    </span>
+    <div class="message-date">
+      <span> [{{ message.date }}] </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      bgColor: this.message.bgColor || '#dad8d8',
+    }
+  },
+  name: "MainMessage",
+  props: ["message"],
+};
+</script>
+
+<style>
+.message-content {
+  display: flex;
+  flex-direction: column;
+  padding: 5px;
+  margin: 5px;
+  border-radius: 10px;
+}
+
+.message-content .message-date {
+  align-self: flex-end;
+  font-size: 0.7rem;
+}
+</style>
